c73178da6c5bea728dd6c2c696a9ae4b044325e6
Hello-Kozloff <hello.kozloff@gmail.com>
2019-10-02 17:38:49 +0300

    feature(single-carousel) added single-carousel component

---
+++ src/blocks/_sections/single-carousel/single-carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $(".single-carousel").flickity({
+        draggable: false,
+        wrapAround: true,
+        prevNextButtons: false,
+        pageDots: false,
+        accessibility: false
+    });
+});
