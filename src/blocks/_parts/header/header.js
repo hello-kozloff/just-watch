b36aee751083dd573d1c0ddf19e097048f4a018f
Hello-Kozloff <hello.kozloff@gmail.com>
2019-10-03 16:16:04 +0300

    feature(header) added mobile search form

--- src/blocks/_parts/header/header.js
+++ src/blocks/_parts/header/header.js
@@ -1,1 +1,18 @@
-$(document).ready(() => { /* header */ });
+$(document).ready(() => {
+
+    //# open mobile search
+    $(".header__action_search").on("click", function (event) {
+        event.preventDefault();
+
+        $(".overlay").addClass("overlay_is-visible");
+        $(".header__site-search").addClass("header__site-search_is-visible");
+    });
+
+    //# close mobile search
+    $(".header__action_close").on("click", function (event) {
+        event.preventDefault();
+
+        $(".overlay").removeClass("overlay_is-visible");
+        $(".header__site-search").removeClass("header__site-search_is-visible");
+    });
+});
