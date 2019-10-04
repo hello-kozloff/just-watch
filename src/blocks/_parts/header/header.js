bf151753e35fbd4fd14584e59e2b38d2a8c978d5
Hello-Kozloff <hello.kozloff@gmail.com>
2019-10-04 12:29:27 +0300

    fix(header) fixed toggle button callbacks

--- src/blocks/_parts/header/header.js
+++ src/blocks/_parts/header/header.js
@@ -1,27 +1,107 @@
-$(document).ready(() => {
+const $actionClass = "header__action";
 
-    //# open mobile search
-    $(".header__action_search").on("click", function (event) {
+//# open hamburger
+$(document).ready(function () {
+    $("." + $actionClass + "_hamburger").on("click", function (event) {
         event.preventDefault();
 
-        $("body").addClass("fixed");
-        $(".overlay").addClass("overlay_is-visible").addClass("overlay_is-contrast");
-        $(".header__site-search").addClass("header__site-search_is-visible");
-    });
-
-    //# close mobile search
-    $(".header__action_close").on("click", function (event) {
-        event.preventDefault();
-        remove();
-    });
-
-    $(".overlay").on("click", function () {
-        remove();
-    });
-
-    function remove() {
-        $("body").removeClass("fixed");
-        $(".overlay").removeClass("overlay_is-visible").removeClass("overlay_is-contrast");
         $(".header__site-search").removeClass("header__site-search_is-visible");
-    }
+
+        const $div = $(".navigation");
+        const $activeClass = "navigation_is-active";
+
+        //# if navigation open
+        if ($div.hasClass($activeClass)) {
+            //# remove overlay
+            removeOverlay(true);
+
+            //# remove active state
+            $div.removeClass($activeClass);
+            return false;
+        }
+
+        //# show overlay
+        showOverlay(false, true);
+
+        //# open navigation
+        $div.addClass($activeClass);
+    });
 });
+
+//# open search
+$(document).ready(function () {
+    $("." + $actionClass + "_search").on("click", function (event) {
+        event.preventDefault();
+
+        $(".navigation").removeClass("navigation_is-active");
+
+        const $div = $(".header__site-search");
+        const $activeClass = "header__site-search_is-visible";
+
+        //# if search open
+        if ($div.hasClass($activeClass)) {
+            //# remove overlay
+            removeOverlay(true);
+
+            //# remove active state
+            $div.removeClass($activeClass);
+            return false;
+        }
+
+        //# show overlay
+        showOverlay(true, true);
+
+        //# open navigation
+        $div.addClass($activeClass);
+    });
+});
+
+//# on click overlay callback
+$(document).ready(function () {
+    $(".overlay").on("click", function () {
+        //# remove overlay and unfreeze body
+        removeOverlay(true);
+
+        //# remove components
+        $(".navigation").removeClass("navigation_is-active");
+        $(".header__site-search").removeClass("header__site-search_is-visible");
+    });
+});
+
+/**
+ * This function show overlay container
+ * @param isContrast
+ * @param isBodyFixed
+ */
+function showOverlay(isContrast = false, isBodyFixed = false) {
+    const $div = $(".overlay");
+
+    $div
+        .removeClass("overlay_is-contrast")
+        .addClass("overlay_is-visible");
+
+    if (isContrast === true) {
+        $div.addClass("overlay_is-contrast");
+    }
+
+    if (isBodyFixed === true) {
+        $("body").addClass("fixed");
+    }
+}
+
+/**
+ * This function remove overlay container
+ * @param unfreezeBody
+ */
+function removeOverlay(unfreezeBody = false) {
+    const $div = $(".overlay");
+
+    $div
+        .removeClass("overlay_is-contrast")
+        .removeClass("overlay_is-visible");
+
+    //# unfreeze
+    if (unfreezeBody === true) {
+        $("body").removeClass("fixed");
+    }
+}
